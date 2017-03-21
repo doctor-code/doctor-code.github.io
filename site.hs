451f428e94235ed3bca0e74ea21a1b51ca89fd13
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-03-21 16:20:30 +0100

    hakyllify project

---
+++ site.hs
@@ -0,0 +1,45 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "font/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "*.md" $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
